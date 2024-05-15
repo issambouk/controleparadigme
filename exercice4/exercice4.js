03548f8052ba1ddb4eb9d7e648e692dff3bb242c
issambouk <issamboukechour@gmail.com>
2024-05-15 16:57:34 +0200

    all

--- exercice4/exercice4.js
+++
@@ -1,8 +0,0 @@
-const filterList = (list) => {
-    return list.filter(item => typeof item === 'number');
-}
-
-
-console.log(filterList([1, 2, 'a', 'b'])); // -> [1, 2]
-console.log(filterList([1, 'a', 'b', 0, 15])); // -> [1, 0, 15]
-console.log(filterList([1, 2, 'aasf', '3', '124', 123])); // -> [1, 2, 123]
